82f370afc30e3b751d3efa8e4425de0c28cf626b
elena.denoeva <denoeva.elena@skyeng.ru>
2023-07-27 19:08:43 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Course3Homework2HogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Homework2HogwartsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course3Homework2HogwartsSchoolApplication.class, args);
+	}
+
+}
